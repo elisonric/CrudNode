955e0f98f0058f47a896b004f2e9977ab690dd77
Elison lamim <elison.ricardo@yahoo.com.br>
2019-08-16 21:43:50 -0300

    Listar - sem acabar

--- client/service.js
+++ client/service.js
@@ -1,42 +1,66 @@
-var server = 'http://localhost:8000/'
+var server = 'http://localhost:8000'
 $(document).ready(function () {
     $("#save").click(function () {
-        $.ajax({
-            url: server + "create",
-            type: 'post',
-            data: {
-                name: "jkjk",
-                role: "jkk"
-            },
-            beforeSend: function () {
-                $("#resultado").html("ENVIANDO...");
-            }
+        axios.post(`${server}/create`, {
+            name: $('#name').val(),
+            role: $('#role').val()
         })
-            .done(function (msg) {
-                console.log(msg);
-                // $("#resultado").html(msg);
+            .then(function (response) {
+                console.log(response);
             })
-            .fail(function (jqXHR, textStatus, msg) {
-                console.log(msg);
+            .catch(function (error) {
+                console.log(error);
             });
     })
 
-    $("#getAll").click(function () {
+    $("#getAll").(function () {
+        axios.get(`${server}/getAll`)
+            .then(function (response) {
+                var list = '<table style="width:60%"><tr><th>Matricula</th><th>Nome</th><th>Cargo</th></tr>';
+                for (let index = 0; index < response.data.list.length; index++) {
+                    const element = response.data.list[index];
+                    console.log(element);
+                    
+                    list += '<tr>'
+                    list += `<td>${element.id}</td>`
+                    list += `<td>${element.obj.name}</td>`
+                    list += `<td>${element.obj.role}</td>`
+                    list += '</tr>'
+                }
+                console.log(list);
+                
+                $("#list").html(list += '</table>')
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    })
 
-        $.ajax({
-            url: server + "getAll",
-            type: 'get',
-            beforeSend: function () {
-                $("#resultado").html("ENVIANDO...");
+    $("#update").click(function () {
+        axios.post(`${server}/updateById`, {
+            id: $("#idUpdate").val(),
+            obj: {
+                name: $('#name').val(),
+                role: $('#role').val()
             }
         })
-            .done(function (msg) {
-                console.log(msg);
-
-                $("#resultado").html(msg);
+            .then(function (response) {
+                console.log(response);
             })
-            .fail(function (jqXHR, textStatus, msg) {
-                console.log(msg);
+            .catch(function (error) {
+                console.log(error);
+            });
+    })
+
+    $("#delete").click(function () {
+        axios.post(`${server}/deleteById`, {
+            id: $("#idDelete").val()
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
             });
     })
 })
